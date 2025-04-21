f088ec11abef522320464d5b6227c1364eead0c8
vladshein <vlshein1@gmail.com>
2025-04-22 01:23:19 +0300

    add solution to task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import Profile from "./Profile/Profile";
 import FriendList from "./FriendList/FriendList";
 import userData from "../userData.json";
 import friends from "../friends.json";
-
+import transactions from "../transactions.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 const App = () => {
   return (
     <>
@@ -15,6 +16,8 @@ const App = () => {
       />
 
       <FriendList friends={friends} />
+      {console.log(transactions)}
+      <TransactionHistory items={transactions} />
     </>
   );
 };
